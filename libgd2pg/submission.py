e860fa863f5950d5295211a19cd719aef6bdb129
Jay Deiman <jay@splitstreams.com>
2020-03-27 08:42:45 -0700

    Adding the stub for a submission library

---
+++ libgd2pg/submission.py
@@ -0,0 +1,12 @@
+# 
+# This is a library for building the JSON to submit to this server
+# This handles key/values and submits them to the specified server
+#
+
+import json
+
+
+class GdataSubmit:
+
+    def __init__(self, username, password):
+        pass
