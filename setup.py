084876ef077b914c5cb320c72aae023209383164
Jay Deiman <jay@splitstreams.com>
2021-10-11 10:39:05 -0700

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements():
 
 setup(
     name='gdata2pg',
-    version='0.1.3',
+    version='0.1.4',
     author='Jay Deiman',
     author_email='admin@splitstreams.com',
     description=(
